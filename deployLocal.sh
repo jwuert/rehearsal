e850dd56f3326c4239550273158e095b7dddad0c
Jan Würthner <wuerthne@shenanigans.speedport.ip>
2025-09-14 18:30:07 +0200

    minor bugfix in cancellation; prepared build file for .war file

--- deployLocal.sh
+++ deployLocal.sh
@@ -1,2 +1,2 @@
-cp build/libs/rehearsal-1.0.0-SNAPSHOT.war /home/wuerthne/Software/apache-tomee-webprofile-10.1.1/webapps/rehearsal.war
+cp build/libs/rehearsal.war /home/wuerthne/Software/apache-tomee-webprofile-10.1.1/webapps/rehearsal.war
 

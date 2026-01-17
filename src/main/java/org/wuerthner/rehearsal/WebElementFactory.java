3986751db7ad296f1194c508507b78642538ced9
Jan Würthner <jan.wuerthner@gmx.de>
2026-01-17 17:30:35 +0100

    incorporated UserProvider; updated UI correspondingly

--- src/main/java/org/wuerthner/rehearsal/WebElementFactory.java
+++ src/main/java/org/wuerthner/rehearsal/WebElementFactory.java
@@ -1,16 +1,33 @@
 package org.wuerthner.rehearsal;
 
 import java.io.File;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Optional;
 
+import jakarta.ejb.EJB;
 import jakarta.ejb.Stateless;
 
+import jakarta.inject.Inject;
 import org.wuerthner.rehearsalmodel.RehearsalFactory;
+import org.wuerthner.sport.api.ActionProvider;
 import org.wuerthner.sport.api.Executor;
 import org.wuerthner.sport.api.ModelElementFactory;
+import org.wuerthner.sport.persistence.dao.GenericDao;
 
 @Stateless
 public class WebElementFactory extends RehearsalFactory implements ModelElementFactory {
+    @Inject
+    public GenericDao genericDao;
+
 	@Override
 	public Optional<Executor<File>> getPdfViewerExecutor() { return Optional.empty(); }
+
+    @Override
+    public Map<String,String> getUserMap() {
+        if (genericDao!=null) {
+            return genericDao.getUserMap();
+        }
+        return new HashMap<>();
+    }
 }
